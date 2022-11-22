7f94217d14a0de9620ebe500416b02874b699424
Giancarlo <93389927+GiancarloRoman@users.noreply.github.com>
2022-11-22 09:46:43 -0500

    Update ComprobanteMonto.java

--- src/main/java/pe/edu/upc/trabajo_grupal1/entities/ComprobanteMonto.java
+++ src/main/java/pe/edu/upc/trabajo_grupal1/entities/ComprobanteMonto.java
@@ -1,1 +1,22 @@
 
+package pe.edu.upc.trabajo_grupal1.entities;
+
+public class ComprobanteMonto {
+    private String cantidad;
+    private String fecha;
+
+    public String getCantidad() { return cantidad; }
+    public void setCantidad(String cantidad) {this.cantidad=cantidad;}
+
+    public String getFecha() { return fecha; }
+    public void setFecha(String fecha) {this.fecha=fecha;}
+
+    public ComprobanteMonto(String cantidad,String fecha) {
+
+        this.cantidad=cantidad;
+        this.fecha=fecha;
+    }
+    public ComprobanteMonto() {
+
+    }
+}
