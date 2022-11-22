fe3b3405256713f9eeac0023c5b5ae206eb75ce5
RenzoPortillo <113268088+RenzoPortillo@users.noreply.github.com>
2022-11-22 10:05:19 -0500

    Update-prenda-repositories

--- src/main/java/pe/edu/upc/trabajo_grupal1/repositories/IPrendaRepository.java
+++ src/main/java/pe/edu/upc/trabajo_grupal1/repositories/IPrendaRepository.java
@@ -13,5 +13,21 @@ public interface IPrendaRepository extends JpaRepository<Prenda,Integer> {
     @Query("from Prenda p where p.nombrePrenda like %:nombrePrenda")
     List<Prenda> searchPrenda(@Param("nombrePrenda") String nombrePrenda);
 
+    @Query("from Prenda p where p.tienda.nombreTienda like %:nombreTienda")
+    List<Prenda> searchTienda(@Param("nombreTienda") String nombreTienda);
 
+    @Query("from Prenda p where p.marca.nombreMarca like %:nombreMarca")
+    List<Prenda> searchMarca(@Param("nombreMarca") String nombreMarca);
+
+    @Query("from Prenda p where p.talla.letraTalla like %:letraTalla")
+    List<Prenda> searchTalla(@Param("letraTalla") String letraTalla);
+
+    @Query(value = "Select nombre_prenda, MAX(precio_unitario) From prenda Group by nombre_prenda", nativeQuery = true)
+    List<String[]> prendaMayorPrecio();
+
+    @Query(value = "Select nombre_prenda, MIN(precio_unitario) From prenda Group by nombre_prenda", nativeQuery = true)
+    List<String[]> prendaMenorPrecio();
+
+    @Query(value ="SELECT nombre_prenda,nombre_marca from prenda p inner join marca m on m.id_marca = p.id_marca",nativeQuery = true)
+    List<String[]>prendaMarca();
 }
