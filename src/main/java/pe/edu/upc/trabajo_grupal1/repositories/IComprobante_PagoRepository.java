945af00ab8ad9ce9b227ba4c9d2bb9330102a26c
Giancarlo <93389927+GiancarloRoman@users.noreply.github.com>
2022-11-22 09:47:45 -0500

    Update IComprobante_PagoRepository.java

--- src/main/java/pe/edu/upc/trabajo_grupal1/repositories/IComprobante_PagoRepository.java
+++ src/main/java/pe/edu/upc/trabajo_grupal1/repositories/IComprobante_PagoRepository.java
@@ -13,5 +13,12 @@ public interface IComprobante_PagoRepository extends JpaRepository<Comprobante_P
     @Query("from Comprobante_Pago cp where cp.tipoComprobante like %:tipoComprobante")
     List<Comprobante_Pago> searchComprobante_Pago_Tipo(@Param("tipoComprobante") String tipoComprobante);
 
+    @Query("from Comprobante_Pago cp where cp.razonsocial like %:razonsocial")
+    List<Comprobante_Pago> searchComprobante_Pago_Razon(@Param("razonsocial") String razonsocial);
 
+    @Query(value = "SELECT fecha ,count (tipo_comprobante) as Cantidad FROM comprobante_de_pago c where c.fecha >'2022-11-20' GROUP BY fecha",nativeQuery = true)
+    List<String[]>comprobanteFecha();
+
+    @Query(value="SELECT fecha,sum(monto_total) as TOTAL_DIARIO from comprobante_de_pago GROUP BY fecha",nativeQuery = true)
+    List<String[]>ComprobanteMonto();
 }
